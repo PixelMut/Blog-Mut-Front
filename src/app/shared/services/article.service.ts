1d70802db21baecb7b644a2f5613e185c4cb05bd
uakgul-talis <uakgul@talis-group.com>
2020-07-09 19:50:53 +0200

    setting up data from node backend

--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -2,21 +2,30 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Article} from '../article';
 import {ARTICLES} from '../mock-articles';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]>{
-    const articles: Article[] = ARTICLES;
-    return of(articles);
+    // internal mockup mode
+    // const articles: Article[] = ARTICLES;
+    // return of(articles);
+
+    // API Mode
+    return this.http.get<Article[]>('http://localhost:8000/articles');
   }
 
   getArticle(key: string): Observable<Article>{
-    const articles: Article[] = ARTICLES.filter(a => a.key === key);
-    return of(articles[0]);
+    // internal mockup mode
+    // const articles: Article[] = ARTICLES.filter(a => a.key === key);
+    // return of(articles[0]);
+
+    // API Mode
+    return this.http.get<Article>('http://localhost:8000/articles/' + key);
   }
 }
